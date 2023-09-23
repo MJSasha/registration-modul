acde03a4f2741c8f140835e00b3cd24dd4f1e311
MJSasha <sasha.ushankov@gmail.com>
2023-09-23 09:58:03 +0300

    Added some props and validations

--- RegistrationModul/ViewModels/MainViewModel.cs
+++ RegistrationModul/ViewModels/MainViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using ReactiveUI;
 using RegistrationModul.Services;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace RegistrationModul.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Login { get; set; }
-    public string Password { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Login { get => login; set => this.RaiseAndSetIfChanged(ref login, value); }
+    [Required]
+    public string Password { get => password; set => this.RaiseAndSetIfChanged(ref password, value); }
 
     public string ErrorMessage { get => errorMessage; set => this.RaiseAndSetIfChanged(ref errorMessage, value); }
     public bool ShowError { get => showError; set => this.RaiseAndSetIfChanged(ref showError, value); }
+
+
+    private string login;
+    private string password;
 
     private bool showError;
     private string errorMessage;
@@ -19,12 +27,6 @@ public partial class MainViewModel : ViewModelBase
     [RelayCommand]
     private async Task SubmitButtonClicked()
     {
-        if (!CheckAllFiLadsFilled())
-        {
-            ErrorMessage = "Not all fields are filled!";
-            ShowError = true;
-            return;
-        }
         var storageService = new StorageService();
         var isUserExist = await storageService.CheckUserExist(Login, Password);
 
@@ -41,10 +43,5 @@ public partial class MainViewModel : ViewModelBase
     private async Task RegistrationButtonClicked()
     {
         Router.Navigate.Execute(new RegistrationViewModel(this));
-    }
-
-    private bool CheckAllFiLadsFilled()
-    {
-        return !string.IsNullOrWhiteSpace(Login) && !string.IsNullOrWhiteSpace(Password);
     }
 }
--- RegistrationModul/ViewModels/RegistrationViewModel.cs
+++ RegistrationModul/ViewModels/RegistrationViewModel.cs
@@ -2,7 +2,7 @@
 using ReactiveUI;
 using RegistrationModul.Models;
 using RegistrationModul.Services;
-using System;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -10,13 +10,28 @@ namespace RegistrationModul.ViewModels
 {
     public partial class RegistrationViewModel : ViewModelBase
     {
-
-        public string Name { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
+        [Required]
+        public string Name { get => name; set => this.RaiseAndSetIfChanged(ref name, value); }
+        [Required]
+        [EmailAddress]
+        public string Login { get => login; set => this.RaiseAndSetIfChanged(ref login, value); }
+        [Required]
+        [RegularExpression(@"^(?=(?:\D*\d){4})[a-zA-Z\d]{6}$", ErrorMessage = "The password must have 6 characters, 4 of which are digits.")]
+        public string Password { get => password; set => this.RaiseAndSetIfChanged(ref password, value); }
+        [Required]
+        [Phone]
+        public string Phone { get => phone; set => this.RaiseAndSetIfChanged(ref phone, value); }
+        [Required]
+        public string Address { get => address; set => this.RaiseAndSetIfChanged(ref address, value); }
 
         public string ErrorMessage { get => errorMessage; set => this.RaiseAndSetIfChanged(ref errorMessage, value); }
         public bool ShowError { get => showError; set => this.RaiseAndSetIfChanged(ref showError, value); }
+
+        private string name;
+        private string login;
+        private string password;
+        private string phone;
+        private string address;
 
         private bool showError;
         private string errorMessage;
@@ -27,15 +42,15 @@ namespace RegistrationModul.ViewModels
         [RelayCommand]
         private async Task RegistrationButtonClicked()
         {
-            if (!CheckAllFiLadsFilled())
-            {
-                ErrorMessage = "Not all fields are filled!";
-                ShowError = true;
-                return;
-            }
-
             var storageService = new StorageService();
-            var user = new User { Name = Name, Login = Login, Password = Password };
+            var user = new User
+            {
+                Name = Name,
+                Login = Login,
+                Password = Password,
+                Phone = Phone,
+                Address = Address,
+            };
 
             try
             {
@@ -47,11 +62,6 @@ namespace RegistrationModul.ViewModels
                 ErrorMessage = ex.Message;
                 ShowError = true;
             }
-        }
-
-        private bool CheckAllFiLadsFilled()
-        {
-            return !string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Login) && !string.IsNullOrWhiteSpace(Password);
         }
     }
 }
