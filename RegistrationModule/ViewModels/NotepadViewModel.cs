d8929fdb214f4ded0d6fb66417a8defb96da22f8
MJSasha <sasha.ushankov@gmail.com>
2023-10-25 22:12:41 +0300

    Added file opening

--- RegistrationModule/ViewModels/NotepadViewModel.cs
+++ RegistrationModule/ViewModels/NotepadViewModel.cs
@@ -1,4 +1,6 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
 using ReactiveUI;
 using RegistrationModul.Models;
 using RegistrationModul.Services;
@@ -9,6 +11,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Threading.Tasks;
 
 namespace RegistrationModul.ViewModels
 {
@@ -16,22 +19,25 @@ namespace RegistrationModul.ViewModels
     {
         #region Public props
 
+        public IStorageFile File { get => file; set { this.RaiseAndSetIfChanged(ref file, value); OnFileOpened(); } }
         public ObservableCollection<UserWitPermission> Users { get => users; set => this.RaiseAndSetIfChanged(ref users, value); }
         public ObservableCollection<string> Roles { get => roles; set => this.RaiseAndSetIfChanged(ref roles, value); }
         public string Text { get => text; set => this.RaiseAndSetIfChanged(ref text, value); }
         public bool CanEdit { get => canEdit; set => this.RaiseAndSetIfChanged(ref canEdit, value); }
+        public bool CanChangeRole { get => canChangeRole; set => this.RaiseAndSetIfChanged(ref canChangeRole, value); }
 
         #endregion
 
         #region Private props
 
-        private readonly string fileName = "text.txt";
         private CompaniesService companiesService;
 
+        private IStorageFile file;
         private ObservableCollection<UserWitPermission> users;
         private ObservableCollection<string> roles;
         private string text;
         private bool canEdit;
+        private bool canChangeRole;
 
         #endregion
 
@@ -56,7 +62,7 @@ namespace RegistrationModul.ViewModels
         [RelayCommand]
         private void Save()
         {
-            File.WriteAllText(fileName, Text);
+            System.IO.File.WriteAllText(file.Path.AbsolutePath, Text);
         }
 
         [RelayCommand]
@@ -87,11 +93,16 @@ namespace RegistrationModul.ViewModels
                 .Select(u => new UserWitPermission { User = u, CanEdit = u.Role == UserRole.Editor }));
 
             Roles = new ObservableCollection<string>(Enum.GetValues<UserRole>().Select(v => v.ToString()));
-            CanEdit = AuthService.CurrentUser.Role == UserRole.Editor;
-            if (File.Exists(this.fileName))
-            {
-                Text = File.ReadAllText(this.fileName);
-            }
+            CanEdit = AuthService.CurrentUser.Role == UserRole.Editor && file != null;
+            CanChangeRole = AuthService.CurrentUser.Role == UserRole.Editor;
+        }
+
+        private async Task OnFileOpened()
+        {
+            using var stream = await file.OpenReadAsync();
+            using var reader = new StreamReader(stream);
+            Text = await reader.ReadToEndAsync();
+            CanEdit = AuthService.CurrentUser.Role == UserRole.Editor && file != null;
         }
     }
 
--- RegistrationModule/Views/NotepadView.axaml.cs
+++ RegistrationModule/Views/NotepadView.axaml.cs
@@ -1,7 +1,11 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
 using RegistrationModul.ViewModels;
+using System.Linq;
 
 namespace RegistrationModul.Views
 {
@@ -11,6 +15,20 @@ namespace RegistrationModul.Views
         {
             this.WhenActivated(disposables => { });
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private async void OpenFileButton_Clicked(object sender, RoutedEventArgs args)
+        {
+            var topLevel = TopLevel.GetTopLevel(this);
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Text File",
+                AllowMultiple = false
+            });
+
+            var file = files.FirstOrDefault();
+            if (file == null) return;
+            ViewModel.File = file;
         }
     }
 }
